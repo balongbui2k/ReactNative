e0e243657b88b7eeeaf650f252b2345a48075bc4
balongbui2k <balongbui2K@gmail.com>
2023-06-09 17:16:34 +0700

    SignIn

--- src/screens/SignInScreen/SignInScreen.js
+++ src/screens/SignInScreen/SignInScreen.js
@@ -1,33 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
-  Image,
   StyleSheet,
-  useWindowDimensions,
   ScrollView,
   KeyboardAvoidingView,
+  useWindowDimensions,
+  Text,
+  TextInput,
 } from 'react-native';
 
 import Logo from '../../assets/images/2khung.png';
-import CustomInput from '../../components/CustomInput';
 import CustomButton from '../../components/CustomButton';
-import {useNavigation} from '@react-navigation/native';
 import {useForm} from 'react-hook-form';
-import axios from 'axios';
 import FastImage from 'react-native-fast-image';
+import auth from '@react-native-firebase/auth';
 
-const SignInScreen = () => {
-  const {height} = useWindowDimensions();
-  const navigation = useNavigation();
-
+const SignInScreen = ({navigation}) => {
   const {
     control,
     handleSubmit,
     formState: {errors},
   } = useForm();
+  const [error, setError] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const {height} = useWindowDimensions();
 
-  const onSignInPressed = () => {
-    navigation.navigate('Home');
+  const onSignInPressed = async () => {
+    if (!email || !password) {
+      setError('Email and password cannot be empty!');
+      return;
+    }
+
+    try {
+      // Assuming `auth` is imported and available in the scope
+      await auth().signInWithEmailAndPassword(email, password);
+      console.warn('User signed in!');
+      navigation.navigate('Home');
+    } catch (error) {
+      if (error.code === 'auth/invalid-email') {
+        setError('Invalid email address!');
+      } else if (error.code === 'auth/user-disabled') {
+        setError('Your account has been disabled!');
+      } else if (error.code === 'auth/user-not-found') {
+        setError('Email is not found');
+      } else if (error.code === 'auth/wrong-password') {
+        setError('Invalid email or password!');
+      } else {
+        console.error(error);
+      }
+    }
   };
 
   const onForgotPasswordPressed = () => {
@@ -36,6 +58,14 @@ const SignInScreen = () => {
 
   const onSignUpPress = () => {
     navigation.navigate('SignUp');
+  };
+
+  const handleGoogleButtonPress = () => {
+    console.warn('onGoogleButtonPress');
+  };
+
+  const handleFacebookButtonPress = () => {
+    console.warn('onFacebookButtonPress');
   };
 
   return (
@@ -51,26 +81,21 @@ const SignInScreen = () => {
             resizeMode="contain"
           />
 
-          <CustomInput
-            name="username"
+          <TextInput
+            value={email}
+            onChangeText={text => setEmail(text)}
             placeholder="Example@gmail.com"
-            control={control}
-            rules={{required: 'Email is required'}}
+            style={styles.input}
           />
-
-          <CustomInput
-            name="password"
+          <TextInput
+            value={password}
+            onChangeText={text => setPassword(text)}
             placeholder="Password"
             secureTextEntry
-            control={control}
-            rules={{
-              required: 'Password is required',
-              minLength: {
-                value: 8,
-                message: 'Password should be minimum 8 characters long',
-              },
-            }}
+            style={styles.input}
           />
+
+          {error !== '' && <Text style={styles.errorText}>*{error}</Text>}
 
           <CustomButton
             text="Sign In"
@@ -82,6 +107,19 @@ const SignInScreen = () => {
             onPress={onForgotPasswordPressed}
             type="TERTIARY"
           />
+
+          <Text>Or you can sign in with</Text>
+
+          <View style={styles.socialButtonContainer}>
+            <CustomButton
+              text="Sign up with Google"
+              onPress={handleGoogleButtonPress}
+            />
+            <CustomButton
+              text="Sign up with Facebook"
+              onPress={handleFacebookButtonPress}
+            />
+          </View>
 
           <CustomButton
             text="Don't have an account? Create one"
@@ -106,6 +144,29 @@ const styles = StyleSheet.create({
     maxHeight: 200,
     marginBottom: 13,
     marginLeft: 30,
+  },
+  errorText: {
+    color: 'red',
+    marginVertical: 3,
+    marginRight: '30%',
+    width: '70%',
+  },
+  socialButtonContainer: {
+    width: 100,
+    height: 100,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  input: {
+    backgroundColor: 'white',
+    width: '100%',
+
+    borderColor: '#e8e8e8',
+    borderWidth: 1,
+    borderRadius: 5,
+
+    paddingHorizontal: 10,
+    marginVertical: 5,
   },
 });
 
