d18dea42dab535c37a20f6533b2d887d9b88dc9d
balongbui2k <balongbui2K@gmail.com>
2023-06-30 18:07:55 +0700

    [fix] images don't appear

--- src/components/CustomCart/FoodCart.js
+++ src/components/CustomCart/FoodCart.js
@@ -1,26 +1,25 @@
+import React, {useState, useEffect} from 'react';
 import {
-  StyleSheet,
-  Text,
   View,
+  Text,
   TouchableOpacity,
-  Image,
   FlatList,
   TouchableWithoutFeedback,
+  StyleSheet,
+  Image,
 } from 'react-native';
-import React from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import Feather from 'react-native-vector-icons/Feather';
+import FastImage from 'react-native-fast-image';
+import {addToCart, removeFromCart} from './../../actions/CartAction';
 import FOOD_DATA from './../../../init_data/foods';
 import {useNavigation} from '@react-navigation/native';
-import {useDispatch, useSelector} from 'react-redux';
-import {addToCart, removeFromCart} from './../../actions/CartAction';
 
 const FoodItem = ({item}) => {
   const dispatch = useDispatch();
 
   const itemsInCart = useSelector(state => state?.cartStore?.carts);
   const itemQuantity = itemsInCart[item?.id.toString()]?.quantity || 0;
-
-  // console.log('itemsInCart>>', JSON.stringify(itemsInCart, null, 2));
 
   const handleAddToCart = () => {
     dispatch(addToCart(item));
@@ -34,7 +33,11 @@ const FoodItem = ({item}) => {
 
   return (
     <View style={styles.foodContainer}>
-      <Image style={styles.image} source={item.image} />
+      <Image
+        style={styles.image}
+        source={{uri: item.image}}
+        resizeMode="contain"
+      />
       <View style={styles.detailContainer}>
         <View>
           <Text style={styles.titleText}>{item.name}</Text>
@@ -66,12 +69,11 @@ const FoodItem = ({item}) => {
     </View>
   );
 };
-
 const renderItem = ({item}) => <FoodItem item={item} />;
 
 export default function FoodCart() {
+  const navigation = useNavigation();
   const cartItems = useSelector(state => state.cartStore?.carts);
-  console.log('cartItems', JSON.stringify(cartItems, null, 2));
 
   const calculateTotalPrice = () => {
     let totalPrice = 0;
@@ -81,16 +83,14 @@ export default function FoodCart() {
     return totalPrice;
   };
 
-  const navigation = useNavigation();
-
-  const onCheckoutPressed = () => {
-    navigation.navigate('OrderScreen');
-  };
-
   const totalQuantity = Object.values(cartItems).reduce(
     (total, item) => total + item.quantity,
     0,
   );
+
+  const onCheckoutPressed = () => {
+    navigation.navigate('OrderScreen');
+  };
 
   return (
     <View style={styles.container}>
@@ -100,7 +100,6 @@ export default function FoodCart() {
         keyExtractor={item => item.id.toString()}
         contentContainerStyle={styles.listContainer}
       />
-
       {/* Order Box */}
       {Object.keys(cartItems).length > 0 && (
         <TouchableWithoutFeedback>
