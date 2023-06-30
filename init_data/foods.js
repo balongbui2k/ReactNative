2c270ff54d7fe17bcc8a6de0c2863dbc52197bf8
balongbui2k <balongbui2K@gmail.com>
2023-06-30 18:07:10 +0700

    [chores] changes image links

--- init_data/foods.js
+++ init_data/foods.js
@@ -4,7 +4,8 @@ const FOOD_DATA = [
     restaurantId: '100',
     name: 'McSpicy Chicken',
     price: 220,
-    image: require('../src/assets/staticImages/gallery/square/sd/mcspicychicken.png'),
+    image:
+      'https://firebasestorage.googleapis.com/v0/b/delivery-app-b198e.appspot.com/o/mcspicychicken.png?alt=media&token=aebcb8b0-2771-4657-8c9a-253e8108237e',
     category: 'Burgers & Wraps',
     description: 'Zesty and redolent whole muscle leg meat patty',
   },
@@ -13,7 +14,8 @@ const FOOD_DATA = [
     restaurantId: '100',
     name: 'McSpicy Panner',
     price: 210,
-    image: require('../src/assets/staticImages/gallery/square/sd/mcspicypaneer.png'),
+    image:
+      'https://firebasestorage.googleapis.com/v0/b/delivery-app-b198e.appspot.com/o/mcspicypaneer.png?alt=media&token=53a1e561-5bfc-426e-9da6-70ea4c1e3aa5',
     category: 'Burgers & Wraps',
     description:
       'Crispy and spicy panner patty with creamy tandoori sauce and crispy lettuce topping.',
@@ -24,7 +26,8 @@ const FOOD_DATA = [
     restaurantId: '100',
     name: 'McChicken',
     price: 160,
-    image: require('../src/assets/staticImages/gallery/square/sd/mcchicken.png'),
+    image:
+      'https://firebasestorage.googleapis.com/v0/b/delivery-app-b198e.appspot.com/o/mcspicychicken.png?alt=media&token=aebcb8b0-2771-4657-8c9a-253e8108237e',
     category: 'Burgers & Wraps',
     description:
       'Batter & breaded chicken patty containing green peas, carrots,',
@@ -35,7 +38,8 @@ const FOOD_DATA = [
     restaurantId: '100',
     name: 'Chicken Maharaja Mac',
     price: 250,
-    image: require('../src/assets/staticImages/gallery/square/sd/chickenmaharajamac.png'),
+    image:
+      'https://firebasestorage.googleapis.com/v0/b/delivery-app-b198e.appspot.com/o/chickenmaharajamac.png?alt=media&token=a652c6f5-29fa-4670-8cb5-218dba247a22',
     category: 'Burgers & Wraps',
     description: 'A double-decker toasted Maharaja bun sandwiched',
   },
@@ -44,26 +48,29 @@ const FOOD_DATA = [
     restaurantId: '100',
     name: 'Pizza McPuff',
     price: 150,
-    image: require('../src/assets/staticImages/gallery/square/sd/pizzamcpuff.png'),
+    image:
+      'https://firebasestorage.googleapis.com/v0/b/delivery-app-b198e.appspot.com/o/pizzamcpuff.png?alt=media&token=2dfd7068-698c-4cf9-9fa5-02641679615f',
     category: 'Burgers & Wraps',
     description:
-      'A blend of assorted vegetables (carrot, beans,capsicum, onion & green peas)',
+      'A blend of assorted vegetables (carrot, beans,capsicum, onion & green peas',
   },
   {
     id: '2006',
     restaurantId: '100',
     name: 'Our World Famous Fries',
     price: 120,
-    image: require('../src/assets/staticImages/gallery/square/sd/ourworldfamousfries.png'),
+    image:
+      'https://firebasestorage.googleapis.com/v0/b/delivery-app-b198e.appspot.com/o/ourworldfamousfries.png?alt=media&token=1025c3b2-b307-400f-bb34-92838c90d31f',
     category: 'Burgers & Wraps',
     description: 'The crisp, fan favorite: our World Famous Fries®',
   },
   {
     id: '2007',
     restaurantId: '100',
-    name: 'Sundae (Chocolate Brownie)',
+    name: 'Sundae (Chocolate Brownie',
     price: 130,
-    image: require('../src/assets/staticImages/gallery/square/sd/sundaechocolatebrownie.png'),
+    image:
+      'https://firebasestorage.googleapis.com/v0/b/delivery-app-b198e.appspot.com/o/sundaechocolatebrownie.png?alt=media&token=360bcc22-adb0-4232-8458-3183ba4cbeeb',
     category: 'Burgers & Wraps',
     description: 'An iconic premium dessert option',
   },
@@ -71,9 +78,10 @@ const FOOD_DATA = [
   {
     id: '2008',
     restaurantId: '100',
-    name: 'McFlurry (Choco Crunch)',
+    name: 'McFlurry (Choco Crunch',
     price: 200,
-    image: require('../src/assets/staticImages/gallery/square/sd/mcflurrychococrunch.png'),
+    image:
+      'https://firebasestorage.googleapis.com/v0/b/delivery-app-b198e.appspot.com/o/mcflurrychococrunch.png?alt=media&token=b8d35a9f-3f22-4c6c-9f9b-929cdd5dc640',
     category: 'Burgers & Wraps',
     description:
       'Milk-based frozen dessert with chocolate crispy and chocolate dip.',
