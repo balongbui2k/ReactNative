0dabc9d60c276017d28b52614338e9a7a880bbfe
balongbui2k <balongbui2K@gmail.com>
2023-07-03 17:03:29 +0700

    [feat] add styles

---
+++ src/components/CustomCart/styles.js
@@ -0,0 +1,134 @@
+
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  //* Item Count text
+  foodContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 4,
+    borderRadius: 8,
+    elevation: 2,
+    backgroundColor: 'white',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 6,
+  },
+  detailContainer: {
+    marginHorizontal: 6,
+    width: '64%',
+  },
+  titleText: {
+    color: 'black',
+    fontWeight: 'bold',
+    width: '86%',
+    fontSize: 16,
+    lineHeight: 16 * 1.4,
+    marginBottom: 8,
+  },
+  descriptionText: {
+    width: '100%',
+    fontSize: 13,
+    lineHeight: 13 * 1.4,
+    marginBottom: 8,
+  },
+  priceText: {
+    color: 'orange',
+    fontWeight: 'bold',
+    fontSize: 16,
+    lineHeight: 14 * 1.4,
+    width: '40%',
+  },
+  footerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginHorizontal: 5,
+    width: '100%',
+  },
+  itemAddContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 8,
+  },
+
+  itemCountText: {
+    color: 'black',
+    fontSize: 16,
+    lineHeight: 15 * 1.4,
+    paddingHorizontal: 6,
+    borderRadius: 3,
+  },
+  //Cart Box-Notification
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  listContainer: {
+    paddingBottom: 64, // Add more space in Flat list
+  },
+  checkoutContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#f8f8f8',
+    padding: 14,
+    borderTopColor: '#ddd',
+  },
+  cartIcon: {
+    marginLeft: 16,
+  },
+  totalQuantityAlert: {
+    position: 'absolute',
+    backgroundColor: 'orange',
+    borderRadius: 10,
+    width: 13,
+    height: 13,
+    justifyContent: 'center',
+    alignItems: 'center',
+    top: 13,
+    left: 26,
+    marginLeft: 25,
+  },
+  totalQuantityText: {
+    fontSize: 7.8,
+    color: 'white',
+    fontWeight: '500',
+    padding: 1,
+    paddingLeft: 1,
+  },
+  cartText: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 16,
+    paddingLeft: 5,
+  },
+  cartTotalPrice: {
+    color: 'orange',
+    fontWeight: '800',
+    fontSize: 16,
+    marginLeft: '32%',
+  },
+  checkoutButton: {
+    marginLeft: 'auto',
+    backgroundColor: 'orange',
+    paddingHorizontal: 8,
+    paddingVertical: 8,
+    borderRadius: 4,
+    marginRight: 16,
+  },
+  checkoutButtonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '700',
+  },
+});
+export default styles
---
+++ src/screens/HomeScreen/styles.js
@@ -0,0 +1,133 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+    borderWidth: 1,
+    marginBottom: -20,
+  },
+  backgroundCurvedContainer: {
+    backgroundColor: '#c84',
+    height: 2000,
+    position: 'absolute',
+    top: -1 * (2000 - 230),
+    width: 2000,
+    borderRadius: 2000,
+    alignSelf: 'center',
+    zIndex: -1,
+  },
+  headerContainer: {
+    justifyContent: 'space-evenly',
+  },
+  locationContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+    marginHorizontal: 20,
+  },
+  locationText: {
+    color: 'white',
+    marginLeft: 5,
+    fontSize: 13,
+    lineHeight: 13 * 1.4,
+  },
+  selectedLocationText: {
+    color: '#ffc',
+    marginLeft: 5,
+    fontSize: 14,
+    lineHeight: 14 * 1.4,
+  },
+  alertBadge: {
+    borderRadius: 32,
+    backgroundColor: '#ffc',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 13,
+    width: 13,
+    position: 'absolute',
+    right: -2,
+    top: -10,
+  },
+  alertBadgeText: {
+    color: '#c84',
+    fontSize: 10,
+    lineHeight: 10 * 1.4,
+    fontFamily: 'bold',
+  },
+  searchContainer: {
+    backgroundColor: 'white',
+    height: 45,
+    borderRadius: 8,
+    marginHorizontal: 20,
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  searchSection: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 10,
+  },
+  searchText: {
+    color: 'grey',
+    fontSize: 16,
+    lineHeight: 16 * 1.4,
+    marginLeft: 10,
+    width: '100%',
+  },
+  categoriesContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    marginTop: 25,
+  },
+  category: {
+    alignItems: 'center',
+  },
+  categoryIcon: isActive => ({
+    height: 31.5,
+    width: 31.5,
+    opacity: isActive ? 1 : 0.3,
+  }),
+  categoryText: isActive => ({
+    fontSize: 11.5,
+    lineHeight: 10 * 1.4,
+    color: 'white',
+    marginTop: 5,
+    opacity: isActive ? 1 : 0.3,
+  }),
+  horizontalListContainer: {
+    marginTop: 30,
+    justifyContent: 'space-around',
+  },
+  listHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginHorizontal: 16,
+    marginVertical: 8,
+  },
+  listHeaderTitle: {
+    color: 'black',
+    fontSize: 16,
+    lineHeight: 16 * 1.4,
+  },
+  listHeaderSubtitle: {
+    color: '#c84',
+    fontSize: 13,
+    lineHeight: 13 * 1.4,
+  },
+  // Logo
+  categoryMenuLogo: {
+    marginHorizontal: 8,
+    marginBottom: 4,
+    marginTop: 4,
+    alignItems: 'center',
+  },
+  activeCategoryMenuLogo: {
+    color: 'white',
+  },
+});
+
+export default styles;
