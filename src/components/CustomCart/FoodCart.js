14566d6fcefa86773086ed92565f7eefe639df62
balongbui2k <balongbui2K@gmail.com>
2023-07-04 14:25:27 +0700

    [chores] OrderBox features

--- src/components/CustomCart/FoodCart.js
+++ src/components/CustomCart/FoodCart.js
@@ -1,17 +1,8 @@
-import React, {useState, useEffect} from 'react';
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  FlatList,
-  TouchableWithoutFeedback,
-  Image,
-} from 'react-native';
+import React from 'react';
+import {View, Text, TouchableOpacity, Image} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import Feather from 'react-native-vector-icons/Feather';
 import {addToCart, removeFromCart} from './../../actions/CartAction';
-import FOOD_DATA from './../../../init_data/foods';
-import {useNavigation} from '@react-navigation/native';
 import styles from '../CustomCart/styles';
 
 export const FoodItem = ({item}) => {
@@ -68,60 +59,24 @@ export const FoodItem = ({item}) => {
     </View>
   );
 };
-const renderItem = ({item}) => <FoodItem item={item} />;
 
-export default function FoodCart() {
-  const navigation = useNavigation();
-  const cartItems = useSelector(state => state.cartStore?.carts);
-
-  const calculateTotalPrice = () => {
-    let totalPrice = 0;
-    Object.values(cartItems).forEach(item => {
-      totalPrice += item.price * item.quantity;
-    });
-    return totalPrice;
-  };
-
-  const totalQuantity = Object.values(cartItems).reduce(
-    (total, item) => total + item.quantity,
-    0,
-  );
-
-  const onCheckoutPressed = () => {
-    navigation.navigate('OrderScreen');
-  };
-
+export const OrderBox = ({totalQuantity, totalPrice, onPress}) => {
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={FOOD_DATA}
-        renderItem={renderItem}
-        keyExtractor={item => item.id.toString()}
-        contentContainerStyle={styles.listContainer}
+    <TouchableOpacity onPress={onPress} style={styles.checkoutContainer}>
+      <Feather
+        name="shopping-cart"
+        size={24}
+        color="orange"
+        style={styles.cartIcon}
       />
-      {/* Order Box */}
-      {Object.keys(cartItems).length > 0 && (
-        <TouchableWithoutFeedback>
-          <View style={styles.checkoutContainer}>
-            <Feather
-              name="shopping-cart"
-              size={24}
-              color="orange"
-              style={styles.cartIcon}
-            />
-            <View style={styles.totalQuantityAlert}>
-              <Text style={styles.totalQuantityText}>{totalQuantity}</Text>
-            </View>
-            <Text style={styles.cartText}>Cart</Text>
-            <Text style={styles.cartTotalPrice}>${calculateTotalPrice()}</Text>
-            <TouchableOpacity
-              style={styles.checkoutButton}
-              onPress={onCheckoutPressed}>
-              <Text style={styles.checkoutButtonText}>Checkout</Text>
-            </TouchableOpacity>
-          </View>
-        </TouchableWithoutFeedback>
-      )}
-    </View>
+      <View style={styles.totalQuantityAlert}>
+        <Text style={styles.totalQuantityText}>{totalQuantity}</Text>
+      </View>
+      <Text style={styles.cartText}>Cart</Text>
+      <Text style={styles.cartTotalPrice}>${totalPrice}</Text>
+      <TouchableOpacity style={styles.checkoutButton} onPress={onPress}>
+        <Text style={styles.checkoutButtonText}>Checkout</Text>
+      </TouchableOpacity>
+    </TouchableOpacity>
   );
-}
+};
