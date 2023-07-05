a2b266f674f362db69f11ae95347ecd8d19f22ce
balongbui2k <balongbui2K@gmail.com>
2023-07-05 14:26:23 +0700

    [style] changes display and button

--- src/screens/OrderScreen/OrderSuccessScreen.js
+++ src/screens/OrderScreen/OrderSuccessScreen.js
@@ -4,22 +4,22 @@ import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
 const OrderSuccessScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
-      {/* <Image
-        source={require('path/to/success-image.png')}
+      <Image
+        source={require('../../assets/images/OrderSuccessIcon.png')}
         style={styles.image}
         resizeMode="contain"
-      /> */}
-      <Text style={styles.title}>Đơn hàng của bạn đã được đặt thành công!</Text>
-      <Text style={styles.subtitle}>Cảm ơn bạn đã mua hàng.</Text>
+      />
+      <Text style={styles.title}>Successfully</Text>
+      <Text style={styles.subtitle}>Just wait food delivery to your home</Text>
       <TouchableOpacity
-        style={styles.button1}
-        onPress={() => navigation.navigate('Home')}>
-        <Text style={styles.buttonText}>Back to Home Screen</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button2}
+        style={styles.buttonOrderHistory}
         onPress={() => navigation.navigate('OrderHistory')}>
         <Text style={styles.buttonText}>Order History</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.buttonHome}
+        onPress={() => navigation.navigate('Home')}>
+        <Text style={styles.buttonText}>Back to Home Screen</Text>
       </TouchableOpacity>
     </View>
   );
@@ -30,42 +30,41 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     paddingHorizontal: 16,
   },
   image: {
-    width: 200,
-    height: 200,
-    marginBottom: 32,
+    width: 250,
+    height: 250,
+    borderRadius: 170,
   },
   title: {
-    fontSize: 24,
-    fontWeight: 'bold',
+    fontSize: 20,
     marginBottom: 16,
     textAlign: 'center',
   },
   subtitle: {
     fontSize: 18,
-    marginBottom: 32,
+    marginBottom: 64,
     textAlign: 'center',
   },
-  button1: {
-    backgroundColor: '#007AFF',
-    borderRadius: 8,
-    paddingHorizontal: 16,
+  buttonOrderHistory: {
+    paddingHorizontal: 130,
     paddingVertical: 12,
+    backgroundColor: '#199',
+    borderRadius: 8,
   },
-  button2: {
-    paddingHorizontal: 16,
-    paddingVertical: 12,
-    backgroundColor: '#007AFF',
+  buttonHome: {
+    backgroundColor: '#199',
     borderRadius: 8,
-    marginTop: 20,
+    paddingHorizontal: 100,
+    paddingVertical: 12,
+    marginTop: 16,
   },
   buttonText: {
     color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
+    fontSize: 16,
+    fontWeight: '600',
     textAlign: 'center',
   },
 });
