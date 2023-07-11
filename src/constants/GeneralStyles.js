6bff2a9dad2dd273720cbff9351ebf92a0dfaff1
balongbui2k <balongbui2K@gmail.com>
2023-07-11 17:17:57 +0700

    [fix] change status bar style

--- src/constants/GeneralStyles.js
+++ src/constants/GeneralStyles.js
@@ -8,7 +8,11 @@ export const hitSlop = {top: 50, bottom: 50, left: 50, right: 50};
 export default CustomStatusBar = () => {
   return (
     <View>
-      <StatusBar barStyle="default" translucent backgroundColor="transparent" />
+      <StatusBar
+        barStyle="dark-content"
+        translucent
+        backgroundColor="transparent"
+      />
       <Separator height={StatusBar.currentHeight} />
     </View>
   );
