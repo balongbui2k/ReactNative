e9477345a57958269c0579d6d6998b7e48498f1c
balongbui2k <balongbui2K@gmail.com>
2023-07-11 17:18:36 +0700

    [fix] small adjust name of screens

--- src/constants/routeNames.js
+++ src/constants/routeNames.js
@@ -1,15 +1,16 @@
 export const ROUTES = {
   AUTHORIZED_STACK: ' AuthorizedStack ',
   HOME: 'Home',
-  RESTAURANT_SCREEN: 'RestaurantScreen',
+  RESTAURANT_DETAILS: 'RestaurantDetails',
   ORDER_SCREEN: 'OrderScreen',
   ORDER_HISTORY: 'OrderHistory',
-  HOME_TABS: 'HomeTabs',
+  BOTTOM_TABS: 'BottomTabs',
   ORDERSUCCESS_SCREEN: 'OrderSuccess',
   //
   UN_AUTHORIZED_STACK: ' UnAuthorizedStack ',
   SIGN_IN_SCREEN: 'SignIn',
   SIGN_UP_SCREEN: 'SignUp',
   FORGOT_PASSWORD_SCREEN: 'ForgotPassword',
+  VERIFICATION_SCREEN: 'VerificationScreen',
   NEW_PASSWORD_SCREEN: 'NewPassword',
 };
